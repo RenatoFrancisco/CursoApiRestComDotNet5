284643a9acaa4cbeaa10a6db8bfc7edb582f44de
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-07-10 00:08:50 -0300

    Validando parâmentros recebidos

--- src/FilmesAPI/Models/Filme.cs
+++ src/FilmesAPI/Models/Filme.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FilmesAPI.Models
 {
     public class Filme
     {
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         public string Diretor { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         public string Genero { get; set; }
+
+        [Range(1, 600, ErrorMessage = "A {0} deve ter no mínimo {1} e no máximo {2} minutos.")]
         public int Duracao { get; set; }
     }
 }
